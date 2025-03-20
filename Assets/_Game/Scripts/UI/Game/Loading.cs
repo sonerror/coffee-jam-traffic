68e82ec6b92526b3ce6be3b6544bec031917d99c
sonerror <nguyenvansonawm@gmail.com>
2025-03-20 17:58:24 +0700

    add asset

---
+++ Assets/_Game/Scripts/UI/Game/Loading.cs
@@ -0,0 +1,36 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    public Image imgFill;
+    public float duration = 2f;
+    private void Awake()
+    {
+        imgFill.fillAmount = 0;
+        RunSlider();
+    }
+
+    public void RunSlider()
+    {
+        DOVirtual.Float(0, 1, duration, (value) =>
+        {
+            imgFill.fillAmount = value;
+        }).SetEase(Ease.InCubic)
+        .OnComplete(() =>
+        {
+            DOVirtual.DelayedCall(1f, () =>
+            {
+                SceneController.Ins.ChangeScene(Const.GAMEPLAY_SCENE, () =>
+                {
+                    UIManager.Ins.OpenUI<MainMenu>();
+                    //LevelManager.Ins.OnLoadLevel();
+                    GameManager.Ins.ChangeState(GameState.MainMenu);
+                });
+            });
+        });
+    }
+
+}
---
+++ Assets/_Game/Scripts/UI/Game/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MainMenu : UICanvas
+{
+    public override void Setup()
+    {
+        base.Setup();
+    }
+    public override void Open()
+    {
+        base.Open();
+    }
+}
